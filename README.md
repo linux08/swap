a1935a0e52ef3b9c9d009da3ab79a32a135f2fc7
David Abimbola <davidabimbola@Davids-iMac.lan>
2023-02-23 02:07:56 -0600

    chore: added readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# swap
+
+## Project setup
+```
+npm install
+```
+
+### To run all test
+```
+npm run test
+```
+
+This module exposes two function getAmount and getPath.
+
+- The getAmount function accept an amount (in USDC) argument and returns the token (COMP) equivalent
+- The getPath function accept an amount (in USDC) argument and returns the optimal path for the trade 
--- dist/constants.js
+++ dist/constants.js
@@ -1,5 +1,6 @@
 export const USDC_TOKEN_ADDRESS = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
 export const COMP_TOKEN_ADDRESS = '0xc00e94cb662c3520282e6f5717214004a7f26888';
+export const WETH_ADDRESS = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 // should be in .env
 export const INFRURA_PROJECT_ID = "XgjAZlimvigOaCf-OUgb2cAJ9BfkfJDO";
 //# sourceMappingURL=constants.js.map
